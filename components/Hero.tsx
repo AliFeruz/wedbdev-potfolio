732990b06fda8f3547e3d7bd334bb5fdd7681a66
Ali Feruz <aliferuzcode@gmail.com>
2024-06-05 19:40:55 +0200

    added grid section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Spotlight } from './ui/Spotlight'
+import { TextGenerateEffect } from './ui/TextEffect'
+import Button from './ui/Button'
+import { FaLocationArrow } from 'react-icons/fa6'
 
 const Hero = () => {
   return (
@@ -10,13 +13,28 @@ const Hero = () => {
         <Spotlight className='top-28 left-80 h-[80vh] w-[50vh]' fill='blue'/>
       </div>
       <div className="h-screen w-full dark:bg-black-100 bg-white  
-      dark:bg-grid-white/[0.2] bg-grid-black/[0.3] relative flex items-center justify-center">
+      dark:bg-grid-white/[0.05] bg-grid-black/[0.3] absolute top-0 left-0 flex items-center justify-center">
       {/* Radial gradient for the container to give a faded look */}
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center
     dark:bg-black-100 bg-white 
     [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"/>
     </div>
-    <div className=''></div>
+    <div className='flex justify-center relative my-20 z-10'>
+        <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
+           <h2 className='uppercase tracking-widest text-xs 
+           text-center text-blue-100 max-w-80'>Dynamic Web Wonder with Next.js</h2> 
+           <TextGenerateEffect className='text-center text-[40px] md:text-5xl lg:text-6xl' 
+           words='Transforming Concepts into Seamless User Experiences'/>
+           <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
+            Hi! I&apos;m Adrian, a Next.js Developer based in Croatia.
+           </p>
+           <a href="#about">
+            <Button text='Download my CV'
+            icon={<FaLocationArrow/>} position='right'
+            classes='uppercase tracking-wide'/>
+           </a>
+        </div>
+    </div>
     </div>
   )
 }
---
+++ types/index.tsx
@@ -0,0 +1,14 @@
+
+export type ItemType = {
+    id: number;
+    title: string;
+    description: string;
+    className: string;
+    imgClassName?: string;
+    titleClassName?: string;
+    img?: string;
+    spareImg?: string;
+    header?: React.ReactNode;
+    icon?: React.ReactNode;
+  };
+  
